c3060aa371d3e393168e4778b99ca3d7e0dbd849
FP-UOC Cristian V <cviaplanaa@fp.uoc.edu>
2020-03-16 21:45:40 +0100

    Icono ayuda añadido. Hace falta crear el componente webview de ayuda

--- app/src/main/java/com/p5m/puzzledroid/MainActivity.java
+++ app/src/main/java/com/p5m/puzzledroid/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
@@ -16,11 +17,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.io.File;
 import java.io.IOException;
@@ -138,6 +143,24 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, PuzzleController.class);
             intent.putExtra("mCurrentPhotoUri", uri.toString());
             startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
